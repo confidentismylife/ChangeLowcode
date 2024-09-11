f4ec44dfef7a36d37e480e7cad8e3c4245611085
pjw <1335407527@qq.com>
2024-09-11 15:19:08 +0800

    d

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('antd')) {
-              // 将 Ant Design 和它的依赖库分到一个独立的 chunk 中
               return 'antd';
             }
             if (id.includes('lodash')) {
               return 'lodash';
             }
-            // 将所有其他来自 node_modules 的模块分到一个单独的 chunk 中
             return 'vendor';
           }
         }
       }
     },
-
-    chunkSizeWarningLimit: 2000 // 设置为 2000KB 或其他合适的值
-  },
-
+    chunkSizeWarningLimit: 2000
+  }
 })
